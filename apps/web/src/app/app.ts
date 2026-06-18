c79635fae1ef052fc34d1b3693cb104a7f6bb18f
David <david.loh18.dl@gmail.com>
2026-06-18 22:59:32 +0200

    reset frontend

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  standalone: false,
+  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'web';
