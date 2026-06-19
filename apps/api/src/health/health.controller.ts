eb3f9af9eb4a582312e806cef1aefaaf38a121d2
David <david.loh18.dl@gmail.com>
2026-06-19 23:26:35 +0200

    database health check

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,18 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, MemoryHealthIndicator } from '@nestjs/terminus';
+import {
+  HealthCheckService,
+  MemoryHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
-@Controller({ version: '1', path: 'health' })
+@Controller({ version: '1', path: '/health' })
 export class HealthController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
-    private readonly memoryHealthIndicator: MemoryHealthIndicator
+    private readonly memoryHealthIndicator: MemoryHealthIndicator,
+    private readonly typeOrmHealthIndicator: TypeOrmHealthIndicator
   ) {}
 
   @Get()
   check() {
     return this.healthCheckService.check([
-      () => this.memoryHealthIndicator.checkHeap('heap', 150 * 1024 * 1024),
-      () => this.memoryHealthIndicator.checkRSS('rss', 150 * 1024 * 1024),
+      () =>
+        this.memoryHealthIndicator.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () =>
+        this.memoryHealthIndicator.checkRSS('memory_rss', 150 * 1024 * 1024),
+      () =>
+        this.typeOrmHealthIndicator.pingCheck('database', { timeout: 3000 }),
     ]);
   }
 }
