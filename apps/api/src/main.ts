fa7c3572981e6ff6cf837e918ff56b6ef7e056eb
David <david.loh18.dl@gmail.com>
2026-06-20 00:56:53 +0200

    configure log level from env file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,26 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { LogLevel, ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const LOG_LEVELS: LogLevel[] = [
+  'fatal',
+  'error',
+  'warn',
+  'log',
+  'debug',
+  'verbose',
+];
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logLevel = LOG_LEVELS.includes(process.env.LOG_LEVEL as LogLevel)
+    ? (process.env.LOG_LEVEL as LogLevel)
+    : 'log';
+
+  const app = await NestFactory.create(AppModule, {
+    logger: [logLevel],
+  });
 
   app.enableVersioning({
     type: VersioningType.URI,
