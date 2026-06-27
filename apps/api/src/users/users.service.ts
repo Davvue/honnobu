bc788340e88b3f206be51ab1648b3a78354d1a45
David <david.loh18.dl@gmail.com>
2026-06-27 21:23:24 +0200

    user service

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../database/entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {}
+
+  async findUserById(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) throw new NotFoundException();
+
+    return user;
+  }
+}
