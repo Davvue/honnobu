241fefe5871e183e59937b785a1d792caf322afe
David <david.loh18.dl@gmail.com>
2026-06-30 10:10:07 +0200

    added shared dto image

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller({ version: '1', path: 'auth' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body() body: LoginDto) {}
 }
---
+++ apps/api/src/auth/dto/login.dto.ts
@@ -0,0 +1,3 @@
+import { ILoginDto } from '@honnobu/dto';
+
+export class LoginDto implements ILoginDto {}
